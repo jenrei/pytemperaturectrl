eabfbf8a6d732cda72c5cd8397a85b0d8960da78
Jentsch Reinhard <Reinhard.Jentsch@ait.ac.at>
2019-03-13 14:10:53 +0100

    minor changes for python3 and julabo connection settings

--- pytemperaturectrl/julabo.py
+++ pytemperaturectrl/julabo.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 julabo.py
 
 Contains Julabo temperature control
  see documentation http://www.julabo.com/sites/default/files/downloads/manuals/french/19524837-V2.pdf at section 10.2.
  :copyright: (c) 2015 by Maxime DAUPHIN
  :license: MIT, see LICENSE for details
-'''
+"""
 
 import serial
 import time
@@ -14,76 +14,75 @@ from .pytemperaturectrl import TemperatureControl
 
 
 class Julabo(TemperatureControl):
-	'''Julabo Temperature control implementation'''
+	"""Julabo Temperature control implementation"""
 	
 	# see Julabo doc
 	MIN_TIME_INTERVAL = 0.250
-	
 	
 	def __init__(self, *args, **kwargs):
 		super(TemperatureControl, self).__init__()
 		self.serial = None 
 		
 	def checkIfOpen(self):
-		''' Check if serial port is open '''
+		""" Check if serial port is open """
 		if self.serial == None:
-			raise Exception('Please call open function before all communication')
+			raise Exception("Please call open function before all communication")
 		
 	def open(self, com_port, baudrate=4800):
-		''' Open serial communication'''
-		self.serial = serial.Serial( com_port,
+		""" Open serial communication"""
+		self.serial = serial.Serial(com_port,
 				 baudrate=baudrate,
-				 bytesize=serial.EIGHTBITS,
-				 parity=serial.PARITY_NONE,
+				 bytesize=serial.SEVENBITS,
+				 parity=serial.PARITY_EVEN,
 				 stopbits=serial.STOPBITS_ONE,
 				 timeout=1,
-				 xonxoff=0,
-				 rtscts=0 ) 
-		
+				 xonxoff=False,
+				 rtscts=True,
+                 dsrdtr=False)
+
 	def close(self):
-		''' Close serial communication'''
+		""" Close serial communication"""
 		self.checkIfOpen()
 		if self.serial != None :
 			self.serial.close()
 			
-	def power( self, on ):
-		'''set power to on or off'''
+	def power(self, on):
+		"""set power to on or off"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
 		value = 1 if on else 0
-		self.serial.write('out_mode_05 %d' % value )
+		self.serial.write(b'f"out_mode_05 {value}\r\n"')
 		
-	def getVersion( self ):
-		'''retrieve engine version'''
+	def getVersion(self):
+		"""retrieve engine version"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
-		self.serial.write('version')
-		return self.serial.readline().strip()
+		self.serial.write(b'version\r\n')
+		return self.serial.readline()
 
-	def getStatus( self ):
-		'''retrieve engine status'''
+	def getStatus(self):
+		"""retrieve engine status"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
-		self.serial.write('status')
-		return self.serial.readline().strip()
+		self.serial.write(b'status\r\n')
+		return self.serial.readline()
 		
-	def setWorkTemperature( self, temperature_in_degree ):
-		'''set setpoint temperature'''
+	def setWorkTemperature(self, temperature_in_degree):
+		"""set setpoint temperature"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
-		self.serial.write( 'out_sp_00 %f' % temperature_in_degree )
+		self.serial.write(b'f"out_sp_00 {temperature_in_degree}\r\n"')
 
-	def getWorkTemperature( self ):
-		'''get setpoint temperature'''
+	def getWorkTemperature(self):
+		"""get setpoint temperature"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
-		self.serial.write( 'in_sp_00' )		
+		self.serial.write(b'in_sp_00\r\n')
 		return float(self.serial.readline())
 
-	def getCurrentTemperature( self ):
-		'''get current tank temperature'''
+	def getCurrentTemperature(self):
+		"""get current tank temperature"""
 		self.checkIfOpen()
 		time.sleep(self.MIN_TIME_INTERVAL)
-		self.serial.write( 'in_pv_00' )		
+		self.serial.write(b'in_pv_00\r\n')
 		return float(self.serial.readline())		
-
